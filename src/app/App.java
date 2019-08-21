0ed5e6820e174696c2dccae76ffe974a08d54d5d
Mugdha-Khatavkar <mugdha_khatavkar@persistent.co.in>
2019-08-21 15:59:07 +0530

    changes after stashing

--- src/app/App.java
+++ src/app/App.java
@@ -3,5 +3,6 @@ public class App{
     public static void main(String args[]){
         System.out.println("Hello java ---- Java is awsome!! Let us use it");
         System.out.println("I am very bored with the training!");
+        System.out.println("kide");
     }
 }
