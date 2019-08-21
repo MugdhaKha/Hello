a72da48859d19eb0f8fb9a90221fc971748c9eef
Mugdha-Khatavkar <mugdha_khatavkar@persistent.co.in>
2019-08-21 14:38:32 +0530

    I am bored line added

--- src/app/App.java
+++ src/app/App.java
@@ -2,5 +2,6 @@ package app;
 public class App{
     public static void main(String args[]){
         System.out.println("Hello java ---- Java is awsome!!");
+        System.out.println("I am very bored with the training!");
     }
 }
